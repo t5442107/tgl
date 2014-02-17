8d14e083c1fbf73c2e64b4134c8842932b89fcb5
yangyuji <yangyuji@yangyuji.local>
2014-02-17 11:42:54 +0800

    Initial Commit

---
+++ tanguilin/tanguilinAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  tanguilinAppDelegate.h
+//  tanguilin
+//
+//  Created by yangyuji on 14-2-17.
+//  Copyright (c) 2014年 com.tanguilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tanguilinAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
